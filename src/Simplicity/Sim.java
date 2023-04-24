032e2e69a881f21f76058d6d7ab1a5e6b6b77275
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2023-04-24 22:59:50 +0700

    Updated Main, World, Sim, and Created invalidMultitudeNumber.java

--- src/Simplicity/Sim.java
+++ src/Simplicity/Sim.java
@@ -587,14 +587,3 @@ class negativeParameterException extends Exception{
     }
 }
 
-class invalidMultitudeNumber extends Exception{
-    private int amount;
-
-    public invalidMultitudeNumber(int amount){
-        this.amount = amount;
-    }
-
-    public String getMessage(){
-        return (amount + " is an invalid number for this operation, please input a different amount");
-    }
-}
