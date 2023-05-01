1973502d14414968064d2f2c0e408e6d0a0a3ed7
Kean1202 <keansantang1202@gmail.com>
2023-05-01 21:02:45 +0700

    small bugs

--- src/Simplicity/tester.java
+++ src/Simplicity/tester.java
@@ -14,7 +14,7 @@ public class tester {
         Toilet toilet = new Toilet("toilet", 100, 1, 1);
         FoodIngredients egg = new FoodIngredients("egg",1 ,2);
         FoodIngredients meat = new FoodIngredients("meat",1 ,2);
-        Sim Kean = new Sim("Kean Nafis Santang", allJobs, world);
+        Sim Kean = new Sim("Kean Nafis Santang", allJobs, world, 0, 0);
         Kean.simInventory.addInventory(egg, 5);
         Kean.simInventory.addInventory(meat, 1);
         FoodCuisine Sushi = new FoodCuisine("Sushi", 5);
